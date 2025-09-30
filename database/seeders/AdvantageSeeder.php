3f1c70efd096811bb01eaffb38a1f97e8fb7c513
athallahdx <athallahgamer4@gmail.com>
2025-09-30 22:49:55 +0700

    update AdvantageSeeder to use new icons for advantages

--- database/seeders/AdvantageSeeder.php
+++ database/seeders/AdvantageSeeder.php
@@ -15,22 +15,22 @@ class AdvantageSeeder extends Seeder
     {
         $advantages = [
             [
-                'icon' => 'advantages/check.png',
+                'icon' => 'advantages/ad1.png',
                 'title' => 'Why Organic Coconut Sugar',
                 'description' => 'We produce our organic coconut sugar in traditional ways, no preservatives added, comes in various taste that will suit your needs.',
             ],
             [
-                'icon' => 'advantages/check.png',
+                'icon' => 'advantages/ad2.png',
                 'title' => 'Benefits',
                 'description' => 'Healthier lifestyle with this low glycemic index sweetener.',
             ],
             [
-                'icon' => 'advantages/check.png',
+                'icon' => 'advantages/ad3.png',
                 'title' => 'Nutrition Facts',
                 'description' => 'Our Organic Coconut Sugar provides the energy and nutrient your body needs for a healthy lifestyle.',
             ],
             [
-                'icon' => 'advantages/check.png',
+                'icon' => 'advantages/ad4.png',
                 'title' => 'Fairer Trade',
                 'description' => 'Means longer and more meaningful partnership. Fairer Trade is a different way of doing business, it’s all about decency and fairness for all parties involved in the supply chain..',
             ]
